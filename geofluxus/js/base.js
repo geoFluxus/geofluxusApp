d546fd5707e4fb4d0892526b52a825d27426e46b
Vasilis <bouzasbasilis@hotmail.com>
2019-12-11 17:45:19 +0100

    sidebar

--- geofluxus/js/base.js
+++ geofluxus/js/base.js
@@ -1,5 +1,8 @@
 // Base
-require(['static/css/base.css', 'static/css/main-navbar.css'],
+require(['static/css/base.css',
+         'static/css/main-navbar.css',
+         'static/css/sidebar.css',
+         '@fortawesome/fontawesome-free/css/all.css'],
 function () {
 
 })
