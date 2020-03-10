16fd6448e3422414c001987d113ec0e4d7164e4d
evertvh <evertvanhirtum@gmail.com>
2020-03-10 15:33:01 +0100

    Implemented Pie chart with test data

--- geofluxus/js/visualizations/piechart.js
+++ geofluxus/js/visualizations/piechart.js
@@ -3,22 +3,59 @@ define([
     'd3-brush',
     'd3plus',
 ], function (d3, d3brush, d3plus) {
-
+    /**
+     *
+     * Pie chart to display Flows data
+     *
+     * @author Evert Van Hirtum
+     */
     class PieChart {
-
+        /**
+         * @param {Object} options          object containing all option values
+         * @param {string} options.el       CSS Selector of the container element of the Pie Chart
+         */
         constructor(options) {
             var options = options || {};
             var _this = this;
 
-            var myData = [
-                {"Group": "Store", "Sub-Group": "Convenience Store", "Number of Food Stores": 100, year: 2018},
-                {"Group": "Store", "Sub-Group": "Grocery Store", "Number of Food Stores": 150, year: 2018},
-                {"Group": "Store", "Sub-Group": "Farmer's Market", "Number of Food Stores": 50, year: 2018},
-                {"Group": "Store", "Sub-Group": "Supercenters", "Number of Food Stores": 30, year: 2018},
-                {"Group": "Restaurant", "Sub-Group": "Fast-Food Restaurant", "Number of Food Stores": 60, year: 2018},
-                {"Group": "Restaurant", "Sub-Group": "Full-Service Restaurant", "Number of Food Stores": 120, year: 2018}
-              ];
-              
+            var myData = [{
+                    "Group": "Store",
+                    "Sub-Group": "Convenience Store",
+                    "Number of Food Stores": 100,
+                    year: 2018
+                },
+                {
+                    "Group": "Store",
+                    "Sub-Group": "Grocery Store",
+                    "Number of Food Stores": 150,
+                    year: 2018
+                },
+                {
+                    "Group": "Store",
+                    "Sub-Group": "Farmer's Market",
+                    "Number of Food Stores": 50,
+                    year: 2018
+                },
+                {
+                    "Group": "Store",
+                    "Sub-Group": "Supercenters",
+                    "Number of Food Stores": 30,
+                    year: 2018
+                },
+                {
+                    "Group": "Restaurant",
+                    "Sub-Group": "Fast-Food Restaurant",
+                    "Number of Food Stores": 60,
+                    year: 2018
+                },
+                {
+                    "Group": "Restaurant",
+                    "Sub-Group": "Full-Service Restaurant",
+                    "Number of Food Stores": 120,
+                    year: 2018
+                }
+            ];
+
 
             new d3plus.Pie()
                 .config({
@@ -38,6 +75,7 @@ define([
                         ]
                     }
                 })
+                // The Pie chart will be rendered in this element:
                 .select(options.el)
                 .render();
 
@@ -49,8 +87,6 @@ define([
         templateFunction() {
 
         }
-
-
     }
     return PieChart;
 });
