978a9de1eee626d3f48e5eddac2964748e618d32
VasileiosBouzas <bouzasbasilis@hotmail.com>
2020-04-03 12:42:28 +0200

    deactivate d3plus

--- geofluxus/js/status-quo.js
+++ geofluxus/js/status-quo.js
@@ -1,17 +1,18 @@
 // Status Quo
-define(['views/status-quo/flows'],
+//define(['views/status-quo/flows'],
+define([],
 function (FlowsView) {
     var flowsView;
 
-    function renderFlows() {
-        flowsView = new FlowsView({
-            el: document.getElementById('flows'),
-            template: 'flows-template'
-        })
-    }
-
+//    function renderFlows() {
+//        flowsView = new FlowsView({
+//            el: document.getElementById('flows'),
+//            template: 'flows-template'
+//        })
+//    }
+//
     function render() {
-        renderFlows();
+        //renderFlows();
     }
 
     render();
