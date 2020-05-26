17abcf7f0b220f2f0ac76b7d02a1149392863a89
Vasilis <bouzasbasilis@hotmail.com>
2020-05-26 10:53:39 +0200

    remove user serializer

--- geofluxus/apps/login/serializers.py
+++ geofluxus/apps/login/serializers.py
@@ -1,25 +1,6 @@
 from rest_framework.serializers import (HyperlinkedModelSerializer,
                                         PrimaryKeyRelatedField)
-from rest_framework.relations import HyperlinkedIdentityField
-from geofluxus.apps.login.models import UserFilter, User
-
-
-# User
-# class UserSerializer(HyperlinkedModelSerializer):
-#     url = HyperlinkedIdentityField(view_name="myapp:user-detail")
-#
-#     class Meta:
-#         model = User
-#         fields = ('url',
-#                   'id',
-#                   'username')
-
-
-# class UserListSerializer(HyperlinkedModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ('id',
-#                   'username')
+from geofluxus.apps.login.models import UserFilter
 
 
 # User filters
