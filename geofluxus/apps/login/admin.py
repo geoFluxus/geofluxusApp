df77f96f289f0d4a6006139e43927c817b39e356
Vasilis <bouzasbasilis@hotmail.com>
2020-06-04 11:28:04 +0200

    publications for user

--- geofluxus/apps/login/admin.py
+++ geofluxus/apps/login/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 from geofluxus.apps.asmfa.admin import CustomAdmin
-from geofluxus.apps.login.models import UserFilter
+from django.contrib.admin import TabularInline
+from geofluxus.apps.login.models import (UserFilter,
+                                         UserPublication,
+                                         PublicationInUser)
 
 
 # User Filter
 @admin.register(UserFilter)
 class UserFilterAdmin(CustomAdmin):
     search_fields = ['user__username']
+
+
+# Publication
+class PublicationInUserInline(TabularInline):
+    model = PublicationInUser
+
+
+# User Publication
+@admin.register(UserPublication)
+class UserPublicationAdmin(CustomAdmin):
+    inlines = (PublicationInUserInline,)
--- geofluxus/apps/login/models.py
+++ geofluxus/apps/login/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from geofluxus.apps.asmfa.models import Publication
 
 
 # Save / edit user filters
@@ -15,3 +16,22 @@ class UserFilter(models.Model):
 
     def __str__(self):
         return f'{self.user}: {self.name} ({self.date})'
+
+
+# Relate users to publications
+class UserPublication(models.Model):
+    user = models.ForeignKey(User,
+                             on_delete=models.CASCADE)
+    publications = models.ManyToManyField(Publication,
+                                          through='PublicationInUser')
+
+    def __str__(self):
+        return f'{self.user}'
+
+
+# PublicationInChain
+class PublicationInUser(models.Model):
+    userpublication = models.ForeignKey(UserPublication,
+                                        on_delete=models.CASCADE)
+    publication = models.ForeignKey(Publication,
+                                    on_delete=models.CASCADE)
