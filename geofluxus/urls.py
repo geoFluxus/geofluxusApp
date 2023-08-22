ed7c5f7d654a63383b91bad101b1624f0b241c11
vasilis <vasilis.bouzas@geofluxus.com>
2023-08-22 16:28:00 +0200

    password reset

--- geofluxus/urls.py
+++ geofluxus/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import re_path, include
+from django.urls import re_path, include, path, reverse_lazy
 from django.conf.urls.static import static
 from django.conf import settings
 from geofluxus.views import HomeView
 from geofluxus.apps.login.views import LoginView, logout_view
+import django.contrib.auth.views as auth_views
 
 urlpatterns = [
     re_path(r'^$', HomeView.as_view(), name='index'),
@@ -33,3 +34,38 @@ urlpatterns = [
 ]\
 + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
 + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += (
+    # default django views
+    # reset password
+    path('password_reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name="commons/password_reset_form.html",
+             subject_template_name="commons/password_reset_subject.txt",
+             email_template_name='commons/password_reset_email.html',
+             success_url=reverse_lazy('password_reset_done'),
+         ),
+         name='password_reset'),
+    path('password_reset_done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name="commons/password_reset_done.html"
+         ),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name="commons/password_reset_confirm.html",
+             success_url=reverse_lazy('password_reset_complete')
+         ),
+         name='password_reset_confirm'),
+    path('password_reset_complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='commons/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
+    # change password
+    path('password_change/',
+         auth_views.PasswordChangeView.as_view(),
+         name='password_change')
+
+    # re_path(r'^', include('django.contrib.auth.urls'))
+)
