7547cc9f1d2a1b589240ac63af7db645682ccaac
vasilis <vasilis.bouzas@geofluxus.com>
2024-10-02 12:59:16 +0200

    minimum password length

--- geofluxus/settings.py
+++ geofluxus/settings.py
@@ -147,6 +147,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+    {
+        'NAME': 'geofluxus.password_validators.SpecialCharactersPasswordValidator'
+    }
 ]
 
 
